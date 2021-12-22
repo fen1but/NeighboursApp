21d939ef07b3865d9b9d794d203baed1ecf01b38
fenibut <fenibut@protonmail.com>
2021-12-22 16:24:26 +0200

    d

--- app/src/main/java/com/example/neighbours/MainActivity.java
+++ app/src/main/java/com/example/neighbours/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.neighbours;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        WormDotsIndicator indicator = findViewById(R.id.indicator);
+
+        int[] list = new int[3];
+        list[0] = 1;
+        list[1] = 2;
+        list[2] = 3;
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this, list);
+        ViewPager2 viewPager2 = findViewById(R.id.viewpager);
+        viewPager2.setAdapter(adapter);
+        indicator.setViewPager2(viewPager2);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/neighbours/ViewPagerAdapter.java
@@ -0,0 +1,49 @@
+package com.example.neighbours;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends RecyclerView.Adapter<ViewPagerAdapter.MyViewHolder> {
+
+    private Context context;
+    private int[] list;
+
+    public ViewPagerAdapter(Context context, int[] list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_viewpager_1, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Toast.makeText(context, ""+position+1, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
